721433a94564a019b49048da9672f4bc4b272870
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 18:40:06 +0200

    Test mit Routen, die sich nie treffen

--- test_gossiping_bus_drivers.py
+++ test_gossiping_bus_drivers.py
@@ -2,12 +2,16 @@ import unittest
 from gossiping_bus_drivers import BusSystem
 
 class TestGossipingBusDrivers(unittest.TestCase):
+    
+    # Test mit nur einer Route:    
     def test_single_route(self):
         bus_system = BusSystem([[1, 2, 3, 4]])
         self.assertEqual(bus_system.run(), 1)
 
+    # Test mit Routen, die sich nie treffen:
     def test_never_meeting_routes(self):
         bus_system = BusSystem([[1, 2, 3, 4], [5, 6, 7, 8]])
         self.assertEqual(bus_system.run(), 'never')
+
 
     # ...weitere Tests später hinzufügen...
