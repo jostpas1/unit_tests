ae6aecf737e5f568d325b7a40dcff0212fbdef75
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 19:58:45 +0200

    # Test mit Mix verschiedenere Situationen

--- test_gossiping_bus_drivers.py
+++ test_gossiping_bus_drivers.py
@@ -53,4 +53,10 @@ class TestGossipingBusDrivers(unittest.TestCase):
         bus_system = BusSystem([[i for i in range(1000)], [999]])
         self.assertEqual(bus_system.run(), 1000)
 
-    # ...weitere Tests später hinzufügen...
+    # Test mit Mix verschiedenere Situationen
+    def test_mixed_situations(self):
+        bus_system = BusSystem([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                                [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                                [100, 90, 80, 70, 60, 50, 40, 30, 20, 10],
+                                [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]])
+        self.assertEqual(bus_system.run(), 'never')
