34577de43520d97c1b66dcfc18d63aca9c5788d9
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 18:22:01 +0200

    Testen mit einer Route

--- test_gossiping_bus_drivers.py
+++ test_gossiping_bus_drivers.py
@@ -2,12 +2,12 @@ import unittest
 from gossiping_bus_drivers import BusSystem
 
 class TestGossipingBusDrivers(unittest.TestCase):
-    def test_single_round(self):
+    def test_single_route(self):
         bus_system = BusSystem([[1, 2, 3, 4]])
         self.assertEqual(bus_system.run(), 1)
 
-class BusSystem:
-    def __init__(self, routes):
-        self.routes = routes
-    def run(self):
-        return 1
+    def test_never_meeting_routes(self):
+        bus_system = BusSystem([[1, 2, 3, 4], [5, 6, 7, 8]])
+        self.assertEqual(bus_system.run(), 'never')
+
+    # ...weitere Tests später hinzufügen...
