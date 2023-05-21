e0af288e4874f07dbf6e778766720622b10e6251
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 19:02:31 +0200

    Überprüfung mehr. Routen, mind. 1x treffen

--- gossiping_bus_drivers.py
+++ gossiping_bus_drivers.py
@@ -6,6 +6,6 @@ class BusSystem:
         # Hinzufügen einer Überprüfung, ob sich die Routen treffen
         for time in range(480):  # Für jede Minute des Tages...
             stops = [route[time % len(route)] for route in self.routes]
-            if len(set(stops)) == len(stops):  # Wenn alle Haltestellen einzigartig sind, treffen sich die Busse nie
-                return 'never'
-        return 1
+            if len(set(stops)) < len(stops):  # Wenn es eine doppelte Haltestelle gibt, treffen sich die Busse
+                return time + 1
+        return 'never'
