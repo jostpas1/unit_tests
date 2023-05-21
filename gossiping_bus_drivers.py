0d7b3904b7798cb979df0b43f0b879722537cd7e
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 18:41:31 +0200

    Hinzufügen Überprüfung, ob sich die Routen treffen

--- gossiping_bus_drivers.py
+++ gossiping_bus_drivers.py
@@ -3,4 +3,9 @@ class BusSystem:
         self.routes = routes
 
     def run(self):
+        # Hinzufügen einer Überprüfung, ob sich die Routen treffen
+        for time in range(480):  # Für jede Minute des Tages...
+            stops = [route[time % len(route)] for route in self.routes]
+            if len(set(stops)) == len(stops):  # Wenn alle Haltestellen einzigartig sind, treffen sich die Busse nie
+                return 'never'
         return 1
