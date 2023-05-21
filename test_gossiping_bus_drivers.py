f3db565935701ac4c49a6659b3c36d97e729fdc6
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-21 18:04:19 +0200

    Implementieren einer Funktion, um den Test zu bestehen

--- test_gossiping_bus_drivers.py
+++ test_gossiping_bus_drivers.py
@@ -5,3 +5,9 @@ class TestGossipingBusDrivers(unittest.TestCase):
     def test_single_round(self):
         bus_system = BusSystem([[1, 2, 3, 4]])
         self.assertEqual(bus_system.run(), 1)
+
+class BusSystem:
+    def __init__(self, routes):
+        self.routes = routes
+    def run(self):
+        return 1
